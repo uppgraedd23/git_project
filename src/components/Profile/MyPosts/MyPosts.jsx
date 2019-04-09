86ae372e5e8cb46f54f0f5380b7cc47cf680fba4
uppgraedd <uppgraedd23@gmail.com>
2019-04-09 10:59:17 +0300

    action_creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+
+
 
 const MyPosts = (props) => {
 
@@ -11,13 +14,13 @@ const MyPosts = (props) => {
 
     let addPost = () => {
         // props.addPost();
-        props.dispatch({type: 'ADD_POST'});
+        props.dispatch(addPostActionCreator());
 
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
         // props.updateNewPostText(text)
-        let action = {type: 'UPDATE_NEW_POST_TEXT', newPost: text};
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
 
     }
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD_POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -73,7 +76,7 @@ let store = {
         this._callSubscriber(this._state);
     },
     dispatch(action) {
-        if (action.type === 'ADD_POST') {
+        if (action.type === ADD_POST) {
 
             let newPost = {
                 id: 5,
@@ -83,14 +86,17 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state)
-        } else if (action.type === 'UPDATE_NEW_POST_TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newPost;
             this._callSubscriber(this._state);
         }
     }
-
-
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({
+        type: UPDATE_NEW_POST_TEXT, newPost: text
+})
 
 export default store;
 window.store = store;
