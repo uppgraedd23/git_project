d13953622feb6168c97bbf675458d9085946fba5
uppgraedd <uppgraedd23@gmail.com>
2019-04-29 15:31:21 +0300

    rest api get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,22 @@
 import React from 'react';
 import styles from './users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/userPhoto.png'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
-                followed: true,
-                fullName: 'Victor',
-                status: 'Baby Im Boss',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            debugger;
+            props.setUsers(response.data.items);
+        })
 
-                followed: false,
-                fullName: 'Vector',
-                status: 'Baby Im Boss more',
-                location: {city: 'Moscow', country: 'Russian'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
-
-                followed: true,
-                fullName: 'Hit Method',
-                status: 'Baby Im Boss very more',
-                location: {city: 'New York', country: 'USA'}
-            },
-            ]
-        )
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
 <span>
     <div>
-        <img src={u.photoUrl} className={styles.userPhoto}/>
+        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
     </div>
     <div>
         {u.followed ? <button onClick={() => {
@@ -52,11 +30,11 @@ let Users = (props) => {
 </span>
 
                 <span>
-    <div>{u.fullName}</div>
+    <div>{u.name}</div>
     <div> {u.status}</div></span>
                 <span>
-    <div>{u.location.city}</div>
-    <div>{u.location.country}</div></span>
+    <div>{"u.location.city"}</div>
+    <div>{"u.location.country"}</div></span>
 
             </div>)
         }
@@ -64,3 +42,32 @@ let Users = (props) => {
 }
 
 export default Users;
+
+// props.setUsers([
+//     {
+//         id: 1,
+//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
+//         followed: true,
+//         fullName: 'Victor',
+//         status: 'Baby Im Boss',
+//         location: {city: 'Kiev', country: 'Ukraine'}
+//     },
+//     {
+//         id: 2,
+//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
+//
+//         followed: false,
+//         fullName: 'Vector',
+//         status: 'Baby Im Boss more',
+//         location: {city: 'Moscow', country: 'Russian'}
+//     },
+//     {
+//         id: 3,
+//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
+//
+//         followed: true,
+//         fullName: 'Hit Method',
+//         status: 'Baby Im Boss very more',
+//         location: {city: 'New York', country: 'USA'}
+//     },
+// ])
