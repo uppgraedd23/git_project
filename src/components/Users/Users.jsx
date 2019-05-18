b719d33b0d3b1ec02645e67e3d5acc87ae249ed5
uppgraedd <uppgraedd23@gmail.com>
2019-05-18 12:24:30 +0300

    class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,75 +3,50 @@ import styles from './users.module.css'
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/userPhoto.png'
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
+class Users extends React.Component {
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 debugger;
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             })
 
         }
     }
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-            props.users.map(u => <div key={u.id}>
+    render(){
+       return  <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
 <span>
     <div>
         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
     </div>
     <div>
         {u.followed ? <button onClick={() => {
-                props.unfollow(u.id)
+                this.props.unfollow(u.id)
             }}>Unfollow</button>
             : <button onClick={() => {
-                props.follow(u.id)
+                this.props.follow(u.id)
             }}>follow</button>}
 
     </div>
 </span>
 
-                <span>
+                    <span>
     <div>{u.name}</div>
     <div> {u.status}</div></span>
-                <span>
+                    <span>
     <div>{"u.location.city"}</div>
     <div>{"u.location.country"}</div></span>
 
-            </div>)
-        }
-    </div>
-}
+                </div>)
+            }
+        </div>
+    }
 
+}
 export default Users;
 
-// props.setUsers([
-//     {
-//         id: 1,
-//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
-//         followed: true,
-//         fullName: 'Victor',
-//         status: 'Baby Im Boss',
-//         location: {city: 'Kiev', country: 'Ukraine'}
-//     },
-//     {
-//         id: 2,
-//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
-//
-//         followed: false,
-//         fullName: 'Vector',
-//         status: 'Baby Im Boss more',
-//         location: {city: 'Moscow', country: 'Russian'}
-//     },
-//     {
-//         id: 3,
-//         photoUrl: 'https://i1.wp.com/theblast.com/wp-content/uploads/2019/03/0319_Travis-Scott.jpg?fit=1024%2C576&ssl=1',
-//
-//         followed: true,
-//         fullName: 'Hit Method',
-//         status: 'Baby Im Boss very more',
-//         location: {city: 'New York', country: 'USA'}
-//     },
-// ])
