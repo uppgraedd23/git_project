5807ee1b6430502144fb0c10daf2c561e3180fc3
uppgraedd <uppgraedd23@gmail.com>
2019-09-02 16:53:38 +0300

    shuldComponentupdate, pureComponent, memo

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWIthHooks.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+
+class ProfileStatus extends React.Component {
+    state = {
+        editMode: false,
+        status: this.props.status
+    }
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deActivateEditMode() {
+        this.setState({
+            editMode: false
+        });
+        this.props.updateStatus(this.state.status);
+    }
+
+    onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevProps.status !== this.props.status) {
+            this.setState(
+                {status: this.props.status}
+            )
+        }
+
+        let a = this.state
+        let b = this.props
+        console.log('componentDidUpdate')
+    }
+
+    render() {
+        console.log('render')
+
+        return (
+
+            <div>
+                {!this.state.editMode &&
+                <div>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status || "---------"}</span>
+                </div>
+                }
+                {this.state.editMode &&
+                <div>
+                    <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deActivateEditMode.bind(this)}
+                           value={this.state.status}/>
+                </div>
+                }
+            </div>
+        )
+    }
+
+
+}
+
+export default ProfileStatus;
